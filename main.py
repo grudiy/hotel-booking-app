8dc5ec310c9ef4996494656133217d43a2d47556
gruand <gruand@gmail.com>
2024-03-08 12:21:36 +0200

    Implement main app design

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv")
+
+
 class Hotel:
-    def view_hotels(self):
+    def __init__(self, id):
         pass
 
     def book(self):
         pass
 
+    def available(self):
+        pass
 
 class Reservation:
-    def generate_reservation(self):
+    def __init__(self, guest_name, hotel_object):
         pass
+
+    def generate(self):
+        pass
+
+
+print(df)
+id = input("Enter id of hotel: ")
+hotel = Hotel(id)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    confirmation = Reservation(name, hotel)
+    print(confirmation.generate())
+else:
+    print("Hotel is not available. Choose another one.")
 
